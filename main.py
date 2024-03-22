a2b605a96d07dd0d514e4dcc83951e16b1042119
Max-Ryujin <max.kannen@gmail.com>
2024-03-22 11:37:43 +0100

    Update API key handling and add API selection option

--- main.py
+++ main.py
@@ -9,17 +9,37 @@ parser.add_argument('-v', '--verbose', action='store_true',
                     help='Increase output verbosity')
 parser.add_argument('-m', '--model', type=str, default="mistralai/Mixtral-8x7B-Instruct-v0.1",
                     help='The model to use for the reasoning task')
+parser.add_argument('-a', '--api', type=str, default="groq", help='The API to use for the reasoning task. select groq or together')
 
 # Parse arguments from the command line
 args = parser.parse_args()
 
+# load GROQ api key and if not available try together api key
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+
 TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
 
-client = openai.OpenAI(api_key=TOGETHER_API_KEY,
-  base_url='https://api.together.xyz',
-)
+
+if args.api == "groq" and GROQ_API_KEY is None:
+    raise ValueError("Please set GROQ_API_KEY in your environment variables")
+elif args.api == "together" and TOGETHER_API_KEY is None:
+    raise ValueError("Please set TOGETHER_API_KEY in your environment variables")
+elif args.api == "groq" and GROQ_API_KEY is not None:
+    client = openai.OpenAI(api_key=GROQ_API_KEY,
+    base_url='https://api.groq.com/openai/v1',
+    )
+    if args.model == "mistralai/Mixtral-8x7B-Instruct-v0.1":
+        args.model = "mixtral-8x7b-32768"
+elif args.api == "together" and TOGETHER_API_KEY is not None:
+    client = openai.OpenAI(api_key=TOGETHER_API_KEY,
+    base_url='https://api.together.xyz',
+    )
+else:
+    raise ValueError("Please set GROQ_API_KEY or TOGETHER_API_KEY in your environment variables")
 
 print("Using model:", args.model)
+print("Using API:", args.api)
 
 def query_llm(prompt, max_tokens=3048):
     messages = [
